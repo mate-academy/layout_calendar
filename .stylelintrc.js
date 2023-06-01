975e4fe0890f6d02d22d221da24247db3de82f3d
Oleh Khalanskyi <okhalanskyi@gmail.com>
2023-06-01 14:30:18 +0300

    feat: calendar

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,10 +1,10 @@
 module.exports = {
   extends: [
-    "stylelint-config-recommended-scss",
+    "stylelint-config-recommended-blocks",
     "@mate-academy/stylelint-config"
   ],
   plugins: [
-    "stylelint-scss"
+    "stylelint-blocks"
   ],
   rules: {}
 };
