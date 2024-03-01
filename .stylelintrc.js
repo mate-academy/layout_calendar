88b9bca2d5b107ca9b2f281d59333f348d142880
Anna <aniasuper1996@gmail.com>
2024-03-01 12:58:54 +0200

    calendar

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -2,8 +2,5 @@ module.exports = {
   extends: "@mate-academy/stylelint-config",
   plugins: [
     "stylelint-scss"
-  ],
-  rules: {
-    "scss/dollar-variable-colon-space-after": "always-single-line"
-  }
+  ]
 };
