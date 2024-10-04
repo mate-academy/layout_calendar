1461560dd603d206e2d49b399e4bc19b8cb94011
Sergiy Lopatkin <lopatkinsergiy@gmail.com>
2024-10-04 13:29:26 +0300

    commit 2

--- backstopConfig.js
+++ backstopConfig.js
@@ -1,5 +1,4 @@
 'use strict';
-// https://github.com/garris/BackstopJS#advanced-scenarios
 
 const backstop = require('@mate-academy/backstop-config');
 const { basicScenario } = backstop;
@@ -8,9 +7,7 @@ const basic = {
   ...basicScenario,
   label: 'Elementary test',
   selectors: ['body'],
-  removeSelectors: [
-    'h1',
-  ],
+  removeSelectors: ['h1'],
   misMatchThreshold: 0.5,
   referenceUrl: basicScenario.referenceUrl + '/calendar/',
 };
@@ -27,28 +24,31 @@ const config = {
       height: 768,
     },
   ],
+  delay: 5000,
   scenarios: [
     {
       ...basic,
       label: 'Calendar initial state',
+      delay: 3000,
     },
     {
       ...basic,
       label: 'Calendar day with hover effect',
       hoverSelector: '.calendar__day:nth-child(5)',
-      postDOMChangeWait: 1000,
+      postDOMChangeWait: 2000,
     },
     {
       ...basic,
       label: 'Calendar starting from Wednesday',
-      postDOMChangeWait: 1000,
+      postDOMChangeWait: 2000,
     },
     {
       ...basic,
       label: 'Calendar with length of 29 days',
-      postDOMChangeWait: 1000,
+      postDOMChangeWait: 2000,
     },
   ],
 };
+
 
 module.exports = config;
