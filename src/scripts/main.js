908044cdb57aa27d60ffe08be3a58ebd1bd7fc45
ab3MN <ab3mn3@gmail.com>
2024-10-14 20:20:40 +0300

    refactor: refactor AI comments

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,9 @@
 'use strict';
+(() => {
+  let template = '';
+  for (let i = 0; i < 31; i++) {
+    template += '<div class="calendar__day"></div>';
+  }
+
+  document.querySelector('.calendar').innerHTML = template;
+})();
